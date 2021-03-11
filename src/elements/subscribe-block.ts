3b5a0804c7f12efc8643b3499bac67c1ffb713ec
Kurniawan Alcantara <kurnhyalcantara@gmail.com>
2021-03-11 09:17:12 +0800

    home/subscribe-block is finish

--- src/elements/subscribe-block.ts
+++ src/elements/subscribe-block.ts
@@ -7,8 +7,9 @@ import { ReduxMixin } from '../mixins/redux-mixin';
 import { RootState, store } from '../store';
 import { openDialog } from '../store/dialogs/actions';
 import { DialogForm, DIALOGS } from '../store/dialogs/types';
-import { subscribe } from '../store/subscribe/actions';
-import { initialSubscribeState, SubscribeState } from '../store/subscribe/state';
+import { requestPermission } from '../store/notifications/actions';
+import { initialNotificationState } from '../store/notifications/state';
+import { NOTIFICATIONS_STATUS } from '../store/notifications/types';
 import './lkim-icons';
 import './shared-styles';
 
@@ -58,15 +59,14 @@ export class SubscribeBlock extends ReduxMixin(PolymerElement) {
         <div class="cta-button">
           <paper-button
             class="animated icon-right"
-            disabled$="[[subscribed.data]]"
-            on-click="_subscribe"
+            on-click="_toggleNotifications"
             ga-on="click"
             ga-event-category="attendees"
             ga-event-action="subscribe"
             ga-event-label="subscribe block"
           >
-            <span class="cta-label">[[ctaLabel]]</span>
-            <iron-icon icon$="lkim:[[ctaIcon]]"></iron-icon>
+            <span class="cta-label">{$  subscribeBlock.callToAction.label $}</span>
+            <iron-icon icon="lkim:arrow-right-circle"></iron-icon>
           </paper-button>
         </div>
       </div>
@@ -74,7 +74,7 @@ export class SubscribeBlock extends ReduxMixin(PolymerElement) {
   }
 
   @property({ type: Object })
-  subscribed: SubscribeState = initialSubscribeState;
+  private notifications: { token?: string; status?: string } = {};
 
   @property({ type: Object })
   private user: { signedIn?: boolean; email?: string; displayName?: string } = {};
@@ -82,53 +82,13 @@ export class SubscribeBlock extends ReduxMixin(PolymerElement) {
   private viewport = {};
 
   stateChanged(state: RootState) {
-    this.subscribed = state.subscribed;
+    this.notifications = state.notifications;
     this.user = state.user;
     this.viewport = state.ui.viewport;
   }
 
-  @computed('subscribed')
-  get ctaIcon() {
-    return this.subscribed instanceof Success ? 'checked' : 'arrow-right-circle';
+  _toggleNotifications() {
+    store.dispatch(requestPermission());
   }
 
-  @computed('subscribed')
-  get ctaLabel() {
-    return this.subscribed instanceof Success
-      ? '{$  subscribeBlock.subscribed $}'
-      : '{$  subscribeBlock.callToAction.label $}';
-  }
-
-  _subscribe() {
-    let userData: {
-      firstFieldValue?: string;
-      secondFieldValue?: string;
-    } = {};
-
-    if (this.user.signedIn) {
-      const fullNameSplit = this.user.displayName.split(' ');
-      userData = {
-        firstFieldValue: fullNameSplit[0],
-        secondFieldValue: fullNameSplit[1],
-      };
-    }
-
-    if (this.user.email) {
-      this._subscribeAction(Object.assign({}, { email: this.user.email }, userData));
-    } else {
-      openDialog(DIALOGS.SUBSCRIBE, {
-        title: '{$ subscribeBlock.formTitle $}',
-        submitLabel: ' {$ subscribeBlock.subscribe $}',
-        firstFieldLabel: '{$ subscribeBlock.firstName $}',
-        secondFieldLabel: '{$ subscribeBlock.lastName $}',
-        firstFieldValue: userData.firstFieldValue,
-        secondFieldValue: userData.secondFieldValue,
-        submit: (data: DialogForm) => this._subscribeAction(data),
-      });
-    }
-  }
-
-  _subscribeAction(data: DialogForm) {
-    store.dispatch(subscribe(data));
-  }
 }
